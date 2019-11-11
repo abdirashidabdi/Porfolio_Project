ce09125d5c7bcd35da01265db2bac206156f064e
Abdirashid.Abdi <student.a.abdi7764@horizoncolumbus.org>
2019-11-11 09:24:46 -0500

    intiall commit

--- Porfolio_Project/urls.py
+++ Porfolio_Project/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('', include('mysite.urls')),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from mysite import views
 from mysite.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mysite/', index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
