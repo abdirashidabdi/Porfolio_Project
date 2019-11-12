8b993f2b7f3905a475a8017ce4bc650701b632b3
Abdirashid.Abdi <student.a.abdi7764@horizoncolumbus.org>
2019-11-12 09:24:58 -0500

    fixed jquery rendering

--- static/content/js/init.js
+++ static/content/js/init.js
@@ -2,11 +2,7 @@
   $(function(){
 
     $('.sidenav').sidenav();
+    $('.slider').slider();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-document.addEventListener('DOMContentLoaded', function() {
-  const elems = document.querySelectorAll('.materialboxed');
-  const instances = M.Materialbox.init(elems, options);
-});
+  });
+})(jQuery);
